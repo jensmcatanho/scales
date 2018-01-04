2e20c0dbaec650e0bd518ede79553a95f7f3135c
Jean Catanho <jensmcatanho@gmail.com>
2018-01-04 00:26:14 -0200

    main: add cli

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+
 	"scales/models"
 	"scales/representations"
+
+	"github.com/urfave/cli"
 )
 
 var (
@@ -54,15 +57,52 @@ func main() {
 
 	initScales()
 
-	fmt.Println("--- Major Scales ---")
-	for _, scale := range majorScales {
-		scale.CheckNotes(os.Args[1:])
+	app := cli.NewApp()
+	app.Flags = []cli.Flag {
+		cli.BoolFlag{
+			Name: "maj, M",
+			Usage: "list only the major scales",
+		},
+		cli.BoolFlag{
+			Name: "min, m",
+			Usage: "list only the natural minor scales",
+		},
+	}
+	
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("maj") {
+			fmt.Println("--- Major Scales ---")
+			for _, scale := range majorScales {
+				scale.CheckNotes(c.Args()[1:])
+			}
+
+			return nil
+			
+		} else if c.Bool("min") {
+			fmt.Println("--- Natural Minor Scales ---")
+			for _, scale := range naturalMinorScales {
+				scale.CheckNotes(c.Args()[1:])
+			}
+			
+			return nil
+
+		} else {
+			fmt.Println("--- Major Scales ---")
+			for _, scale := range majorScales {
+				scale.CheckNotes(c.Args()[1:])
+			}
+		
+			fmt.Println("--- Natural Minor Scales ---")
+			for _, scale := range naturalMinorScales {
+				scale.CheckNotes(c.Args()[1:])
+			}
+			
+			return nil
+		}
 	}
 
-	fmt.Println("--- Natural Minor Scales ---")
-	for _, scale := range naturalMinorScales {
-		scale.CheckNotes(os.Args[1:])
-	}
+	app.Run(os.Args)
+
 	/*
 		fmt.Println("--- Harmonic Minor Scales ---")
 		for _, scale := range harmonicMinorScales {
