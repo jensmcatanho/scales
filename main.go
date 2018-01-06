c37302e37582d0202836e57b87b68202cf78f159
Jean Catanho <jensmcatanho@gmail.com>
2018-01-06 02:30:53 -0200

    main: add support to melodic and harmonic minor scales

--- main.go
+++ main.go
@@ -13,23 +13,42 @@ import (
 )
 
 var (
-	majorScales         []models.Scale
-	naturalMinorScales  []models.Scale
-/*
+	majorScales        []models.Scale
+	naturalMinorScales []models.Scale
 	harmonicMinorScales []models.Scale
 	melodicMinorScales  []models.Scale
-*/
 )
 
-func initScales() {
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal(err)
+func loadScales(fp ...string) (scales []representations.Scale, err error) {
+	for _, file := range fp {
+		scalesRepresentation := []representations.Scale{}
+		var configFile *os.File
+
+		configFile, err = os.Open(file)
+		if err != nil {
+			return
+		}
+
+		decoder := json.NewDecoder(configFile)
+		err = decoder.Decode(&scalesRepresentation)
+		if err != nil {
+			return
+		}
+
+		scales = append(scales, scalesRepresentation...)
 	}
 
-	decoder := json.NewDecoder(configFile)
-	scales := []representations.Scale{}
-	err = decoder.Decode(&scales)
+	return
+}
+
+func initScales() {
+
+	scales, err := loadScales(
+		"config/major.json",
+		"config/naturalMinor.json",
+		"config/harmonicMinor.json",
+		"config/melodicMinor.json",
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,12 +59,10 @@ func initScales() {
 			majorScales = append(majorScales, models.NewScaleFromRepresentation(scale))
 		case "Natural Minor":
 			naturalMinorScales = append(naturalMinorScales, models.NewScaleFromRepresentation(scale))
-/*
 		case "Harmonic Minor":
 			harmonicMinorScales = append(harmonicMinorScales, models.NewScaleFromRepresentation(scale))
 		case "Melodic Minor":
 			melodicMinorScales = append(melodicMinorScales, models.NewScaleFromRepresentation(scale))
-*/
 		}
 	}
 }
@@ -54,21 +71,21 @@ func main() {
 	initScales()
 
 	app := cli.NewApp()
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 		cli.BoolFlag{
-			Name: "maj, M",
+			Name:  "maj, M",
 			Usage: "list only the major scales",
 		},
 		cli.BoolFlag{
-			Name: "min, m",
+			Name:  "min, m",
 			Usage: "list only the natural minor scales",
 		},
 		cli.BoolFlag{
-			Name: "enharmonic, e",
+			Name:  "enharmonic, e",
 			Usage: "list notes from the scales with their enharmonics",
 		},
 	}
-	
+
 	app.Action = func(c *cli.Context) error {
 		if c.Bool("maj") {
 			fmt.Println("--- Major Scales ---")
@@ -77,13 +94,13 @@ func main() {
 			}
 
 			return nil
-			
+
 		} else if c.Bool("min") {
 			fmt.Println("--- Natural Minor Scales ---")
 			for _, scale := range naturalMinorScales {
 				scale.CheckNotes(c.Args(), c.Bool("enharmonic"))
 			}
-			
+
 			return nil
 
 		} else if len(c.Args()) < 1 {
@@ -96,26 +113,24 @@ func main() {
 		for _, scale := range majorScales {
 			scale.CheckNotes(c.Args(), c.Bool("enharmonic"))
 		}
-	
+
 		fmt.Println("--- Natural Minor Scales ---")
 		for _, scale := range naturalMinorScales {
 			scale.CheckNotes(c.Args(), c.Bool("enharmonic"))
 		}
-		
-		return nil
-	}
 
-	app.Run(os.Args)
-
-	/*
 		fmt.Println("--- Harmonic Minor Scales ---")
 		for _, scale := range harmonicMinorScales {
-			scale.CheckNotes(os.Args[1:])
+			scale.CheckNotes(c.Args(), c.Bool("enharmonic"))
 		}
 
 		fmt.Println("--- Melodic Minor Scales ---")
 		for _, scale := range melodicMinorScales {
-			scale.CheckNotes(os.Args[1:])
+			scale.CheckNotes(c.Args(), c.Bool("enharmonic"))
 		}
-	*/
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
